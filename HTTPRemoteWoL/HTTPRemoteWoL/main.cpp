a0f1ea7578a7c293de2f03469d45334a7f77a29d
musialny.dev <musialny.dev@gmail.com>
2021-10-09 16:41:08 +0200

    Refactorization

--- HTTPRemoteWoL/HTTPRemoteWoL/main.cpp
+++ HTTPRemoteWoL/HTTPRemoteWoL/main.cpp
@@ -6,13 +6,10 @@
 #include "WoL.h"
 #include "EEPROMStorage.h"
 #include "FlashStorage.h"
-#include "Utilities.h"
 
 constexpr int FACTORY_RESET_PIN = 8; // Push high state
 constexpr int STATUS_PIN = 9;
 
-constexpr const int woLDefaultAddressListAmount = 1;
-constexpr const byte woLDefaultAddressList[woLDefaultAddressListAmount][6] = {{ 0x18, 0xC0, 0x4D, 0x85, 0x10, 0x2F }};
 namespace woLDefaultAddressNames {
 	const char PC[] PROGMEM = "PC";
 }
@@ -21,14 +18,16 @@ WoL::WoLHandler* wolHandler;
 HTTPServer* httpServer;
 
 void setup() {
+	constexpr const int woLDefaultAddressListAmount = 1;
+	constexpr const byte woLDefaultAddressList[woLDefaultAddressListAmount][6] = {{ 0x18, 0xC0, 0x4D, 0x85, 0x10, 0x2F }};
+	FlashStorage<char> woLNames[woLDefaultAddressListAmount] = {woLDefaultAddressNames::PC};
+	constexpr const byte deviceMac[6] = {0xD0, 0xD4, 0xC3, 0x71, 0x82, 0x19};
+	constexpr const byte broadcastAddress[4] = { 10, 10, 0, 255 };
+	
 	pinMode(FACTORY_RESET_PIN, INPUT);
 	if (digitalRead(FACTORY_RESET_PIN)) EEPROMStorage::formatStorage();
 	
-	FlashStorage<char> woLNames[woLDefaultAddressListAmount] = {woLDefaultAddressNames::PC};
 	EEPROMStorage::initStorage(20, 100, woLDefaultAddressList, woLNames, woLDefaultAddressListAmount);
-	
-	const byte deviceMac[6] = {0xC0, 0x06, 0x42, 0xC4, 0x40, 0x9D};
-	const byte broadcastAddress[4] = { 10, 10, 0, 255 };
 	httpServer = new HTTPServer(deviceMac, IPAddress(10, 10, 0, 10), 80, STATUS_PIN);
 	httpServer->use(Middlewares::auth())
 		.use(Middlewares::homePage())
